171e48fe387334401da07a0cda76665834275614
Luan Solino <luansolino@gmail.com>
2020-03-18 22:32:22 -0300

    Controller and seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,129 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Questão 1
+puts "Criando 1ª questão..."
+Question.create!(
+  enunciated: 'Costumo visitar com regularidade pessoas relevantes em minha vida como amigos, parentes, filhos. '
+)
+puts "1ª questão criada!"
+
+# Questão 2
+puts "Criando 2ª questão..."
+Question.create!(
+  enunciated: 'Costumo participar de reuniões sem saber direito o conteúdo, o porquê devo participar ou a que resultado aquele encontro pode levar.'
+)
+puts "2ª questão criada!"
+
+# Questão 3
+puts "Criando 3ª questão..."
+Question.create!(
+  enunciated: 'Posso afirmar que estou conseguindo realizar tudo que gostaria em minha vida e que o tempo está passando na velocidade correta.'
+)
+puts "3ª questão criada!"
+
+# Questão 4
+puts "Criando 4ª questão..."
+Question.create!(
+  enunciated: 'Nos dias de descanso, costumo passar boa parte do dia assistindo televisão, jogando ou acessando a Internet. '
+)
+puts "4ª questão criada!"
+
+# Questão 5
+puts "Criando 5ª questão..."
+Question.create!(
+  enunciated: 'Leio muitos e-mails desnecessários, com piadas, correntes, propagandas, apresentações, produtos, etc. '
+)
+puts "5ª questão criada!"
+
+# Questão 6
+puts "Criando 6ª questão..."
+Question.create!(
+  enunciated: 'É comum aparecerem problemas inesperados no meu dia a dia. '
+)
+puts "6ª questão criada!"
+
+# Questão 7
+puts "Criando 7ª questão..."
+Question.create!(
+  enunciated: 'Faço esporte com regularidade, me alimento da forma adequada e tenho o lazer que gostaria. '
+)
+puts "7ª questão criada!"
+
+# Questão 8
+puts "Criando 8ª questão..."
+Question.create!(
+  enunciated: 'Faço um planejamento por escrito de tudo que preciso fazer durante minha semana.  '
+)
+puts "8ª questão criada!"
+
+# Questão 9
+puts "Criando 9ª questão..."
+Question.create!(
+  enunciated: 'Costumo deixar para fazer relatórios, imposto de renda, compras de natal, estudar para provas e outras tarefas perto do prazo de entrega. '
+)
+puts "9ª questão criada!"
+
+# Questão 10
+puts "Criando 10ª questão..."
+Question.create!(
+  enunciated: 'Assumo compromissos com outras pessoas ou aceito novas posições na empresa, mesmo que não goste muito da nova atividade, se for para aumentar meus rendimentos ou obter uma promoção. '
+)
+puts "10ª questão criada!"
+
+# Questão 11
+puts "Criando 11ª questão..."
+Question.create!(
+  enunciated: 'É comum reduzir meu horário de almoço ou até mesmo comer enquanto trabalho para concluir um projeto ou tarefa. '
+)
+puts "11ª questão criada!"
+
+# Questão 12
+puts "Criando 12ª questão..."
+Question.create!(
+  enunciated: 'Quando quero alguma coisa, defino esse objetivo por escrito, estabeleço prazos em minha agenda, monitoro os resultados obtidos e os comparo com os esperados.  '
+)
+puts "12ª questão criada!"
+
+# Questão 13
+puts "Criando 13ª questão..."
+Question.create!(
+  enunciated: 'Consigo melhores resultados e me sinto mais produtivo quando estou sob pressão ou com o prazo curto.  '
+)
+puts "13ª questão criada!"
+
+# Questão 14
+puts "Criando 14ª questão..."
+Question.create!(
+  enunciated: 'Quando recebo um novo e-mail, costumo dar uma olhada para checar o conteúdo. '
+)
+puts "14ª questão criada!"
+
+# Questão 15
+puts "Criando 15ª questão..."
+Question.create!(
+  enunciated: 'Tenho um tempo definido para dedicar a mim mesmo e nele, posso fazer o que quiser.  '
+)
+puts "15ª questão criada!"
+
+# Questão 16
+puts "Criando 16ª questão..."
+Question.create!(
+  enunciated: 'Estive atrasado com minhas tarefas ou reuniões nas últimas semanas  '
+)
+puts "16ª questão criada!"
+
+# Questão 17
+puts "Criando 17ª questão..."
+Question.create!(
+  enunciated: 'Costumo ir a eventos, festas ou cursos, mesmo sem ter muita vontade, para agradar meu chefe, meus amigos ou família.  '
+)
+puts "17ª questão criada!"
+
+# Questão 18
+puts "Criando 18ª questão..."
+Question.create!(
+  enunciated: 'Não consigo realizar tudo que me propus fazer no dia e preciso fazer hora extra ou levar trabalho para casa. '
+)
+puts "18ª questão criada!"
